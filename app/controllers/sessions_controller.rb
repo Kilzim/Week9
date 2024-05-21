0e515a863a83d674d95fbd7521d1ff528700a91a
Kilian Zimmermann <kilian.zimmermann@kellogg.northwestern.edu>
2024-05-21 09:44:04 +0000

    1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,28 @@
 class SessionsController < ApplicationController
   def new
+
   end
   
   def create
     # TODO: authenticate user
-    flash["notice"] = "Nope."
-    redirect_to "/login"
+    @user = User.find_by({"email" => params["email"]})
+    if @user != nil
+      if @user["password"] == params["password"]
+        flash["notice"] = "Welcome"
+        redirect_to "/posts"
+       else
+        flash["notice"] = "Nope"
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Nope"
+      redirect_to "/login"
+    end
   end
+
+  def destroy
+
+  end
+
 end
+
